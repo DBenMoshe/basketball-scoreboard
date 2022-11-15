31eb40f0ea81c423a58030f1bc33df308e3cbc35
David Ben Moshe <110648029+DBenMoshe@users.noreply.github.com>
2022-11-15 05:48:24 +0200

    exposed function to window object

--- index.js
+++ index.js
@@ -16,23 +16,32 @@ function homeTwo() {
     homePoints.textContent = hPoints
 }
 
+window.homeTwo = homeTwo
+
 function homeThree() {
     hPoints += 3
     homePoints.textContent = hPoints
 }
+
+window.homeThree = homeThree
 
 function guestOne() {
     gPoints++
     guestPoints.textContent = gPoints
 }
 
+window.guestOne = guestOne
+
 function guestTwo() {
     gPoints += 2
     guestPoints.textContent = gPoints
 }
+
+window.guestTwo = guestTwo
 
 function guestThree() {
     gPoints += 3
     guestPoints.textContent = gPoints
 }
 
+window.guestThree = guestThree
